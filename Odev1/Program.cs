abf9d2147497eb2da93017039bd79f3dcacc1fe4
burakoz <burakoz@BURAKOZ>
2022-10-15 21:03:07 +0300

    Proje dosyası ekle.

---
+++ Odev1/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Odev1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Öğrenci Vize Final Notu Hesaplama");
+
+            // Console'^dan girilen vize ve final notları ile ortalamayı hesaplayan bu ortalamaya göre harfnotunu bulup ekrana ortalama ve harfnotunu yazan prog. yazınız.. tanımladığım değişkenleri kullanınız... 
+
+
+            float vize, final, ort=0;
+
+            // vize notu isteyiniz
+
+            // final notu isteyiniz
+
+            // ortalama hesaplayınız
+            // ortalam vize notunun %40 finalnotunun %60 formülü ile hesaplanır. Formül (vizenotu * 0.4) + (finalnotu * 0.6)
+            Console.WriteLine("Vize Notunu Giriniz");
+            vize = Convert.ToInt32(Console.ReadLine());
+
+            vize = vize / 100 * 40;
+            Console.WriteLine("Final  notunu giriniz");
+            final = Convert.ToInt32(Console.ReadLine());
+            final = final / 100 * 60;
+
+            ort = vize + final;
+
+            string harfNotu="";
+            if (ort >=85 && ort <=100)
+            {
+                harfNotu = "AA";
+            }
+            else if (ort >= 75 && ort <= 84)
+            {
+                harfNotu = "BA";
+            }
+            else if (ort >= 65 && ort <= 74)
+            {
+                harfNotu = "BB";
+            }
+            else if (ort >= 50 && ort <= 64)
+            {
+                harfNotu = "CC";
+            }
+            else if (ort >= 25 && ort <= 49)
+            {
+                harfNotu = "DC";
+            }
+
+            else
+            {
+                harfNotu = "FF";
+            }
+            // harfNotu =  Eğer ortalama 85 ile 100 arasında ise AA, 75 ile 84 arasında ise BA,65 ile 74 arasında ise BB,50 ile 64 arasında ise CC,25 ile 49 arasında ise DC, 25'ten küçük ise FF olarak bulunmalıdır..
+
+            Console.WriteLine($"Ortalamanız ={ort} HarfNotunuz = {harfNotu}");
+
+
+            // todo: bütün  istenilenleri switch-case ve if karar mekanizmaları kullanarak yapınız
+
+
+            Console.WriteLine("Kapatmak  için bir tuşa basınız");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Odev2/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace Odev2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Console'dan alınan ay'ın mevsimini yazdıran prorgramı yapınız.
+            Console.WriteLine("Lütfen ay giriniz");
+            string ay = Console.ReadLine();
+
+            switch (ay)
+            {
+
+
+                case "aralık":
+                    Console.WriteLine("Mevsim Kış");
+                    Console.WriteLine("12.ay");
+                    break;
+                case "ocak":
+                    Console.WriteLine("Mevsim Kış");
+                    Console.WriteLine("1.ay");
+                    break;
+                case "şubat":
+                    Console.WriteLine("Mevsim Kış");
+                    Console.WriteLine("2.ay");
+                    break;
+                case "mart":
+                    Console.WriteLine("Mevsim İlkbahar");
+                    Console.WriteLine("3.ay");
+                    break;
+                case "nisan":
+                    Console.WriteLine("Mevsim İlkbahar");
+                    Console.WriteLine("4.ay");
+                    break;
+                case "mayıs":
+                    Console.WriteLine("Mevsim İlkbahar");
+                    Console.WriteLine("5.ay");
+                    break;
+                case "haziran":
+                    Console.WriteLine("Mevsim Yaz");
+                    Console.WriteLine("6.ay");
+                    break;
+                case "temmuz":
+                    Console.WriteLine("Mevsim Yaz");
+                    Console.WriteLine("7.ay");
+                    break;
+                case "agustos":
+                    Console.WriteLine("Mevsim Yaz");
+                    Console.WriteLine("8.ay");
+                    break;
+                case "eylul":
+                    Console.WriteLine("Mevsim Sonbahar");
+                    Console.WriteLine("9.ay");
+                    break;
+                case "ekim":
+                    Console.WriteLine("Mevsim Sonbahar");
+                    Console.WriteLine("10.ay");
+                    break;
+                case "kasım":
+                    Console.WriteLine("Mevsim Sonbahar");
+                    Console.WriteLine("11.ay");
+                    break;
+
+                default:
+                    break;
+            }
+
+
+            // eğer ay;  aralık-ocak-şubat      girilirse ekrana KIŞ 
+            // mart-nisan-mayıs       girilirse ekrana İLK BAHAR
+            // haziran-temmuz-ağustos girilirse ekrana YAZ 
+            // eylük-ekim-kasım       girilirse ekrana SON BAHAR
+            // mesajlarını veren programı yazınız.. Girilen ay için kaçıncı ay olduunu yazınız ekrana n. Ay şeklinde yazınız..
+
+            // örnek;
+            // klavyeden girilen ocak ayı için çıktı aşağıdaki gibi olmalıdır;
+            // KIŞ
+            // 1. Ay
+
+
+            // todo: bütün  istenilenleri switch-case ve if karar mekanizmaları kullanarak yapınız
+
+            Console.WriteLine("Kapatmak için bir tuşa basınız");
+            Console.ReadKey();
+        }
+    }
+}
